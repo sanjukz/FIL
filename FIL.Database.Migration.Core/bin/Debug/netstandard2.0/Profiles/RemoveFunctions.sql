13566de2d79122d9bb22cca7537442391af5431b
sanjst <chavansanjiv101@gmail.com>
2020-09-03 01:28:23 +0530

    Chages

---
+++ FIL.Database.Migration.Core/bin/Debug/netstandard2.0/Profiles/RemoveFunctions.sql
@@ -0,0 +1,15 @@
+﻿declare @funcName varchar(500)
+declare cur cursor 
+
+for select [name] from sys.objects where type = 'fn'
+open cur
+fetch next from cur into @funcName
+while @@fetch_status = 0
+begin
+    exec('drop function [' + @funcName + ']')
+    fetch next from cur into @funcName
+end
+close cur
+deallocate cur
+
+GO
